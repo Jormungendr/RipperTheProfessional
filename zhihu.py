c0fe5db087a26042ffb2956db3bfcce4ff76ecc9
Zitong Tian <zitong.tian@forceclouds.com>
2017-11-28 18:04:44 +0800

    update gitignore

--- zhihu.py
+++ zhihu.py
@@ -7,3 +7,14 @@ import newspaper
 Client = zhihu_oauth.ZhihuClient()
 Client.load_token('token.pkl')
 me = Client.me()
+question = Client.question(68546899)
+with open('question_68546899_result.txt','w') as f:
+    for i in question.answers:
+        tr4w = TextRank4Keyword()
+        tr4w.analyze(text=i.content, lower=True, window=2)
+        f.write('<--------------------关键词-------------------->\n')
+        for item in tr4w.get_keywords(20, word_min_len=1):
+            f.write(str(item.word) +'  '+ str(item.weight)+'\n')
+        f.write('<--------------------关键短语-------------------->\n')
+        for phrase in tr4w.get_keyphrases(keywords_num=20, min_occur_num=2):
+            f.write(phrase+'\n')
